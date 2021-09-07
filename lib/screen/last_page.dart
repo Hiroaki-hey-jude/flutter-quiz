a7a4123156b6fcf3af0c500e626709b22bca1f4f
Hiroaki Ueda <hiroakiueda2000@gmail.com>
2021-09-07 20:27:33 +0900

    クイズが終わった後もう一回プレイできるように改良しました

--- lib/screen/last_page.dart
+++ lib/screen/last_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/screen/question_page.dart';
 
 class LastPage extends StatefulWidget {
   int num = 0;
+
   LastPage(this.num);
+
   @override
   _LastPageState createState() => _LastPageState();
 }
@@ -13,8 +16,31 @@ class _LastPageState extends State<LastPage> {
     return Scaffold(
       backgroundColor: Colors.blueAccent,
       body: Center(
-        child: Container(
-          child: Text('おめでとう${widget.num}個成果したよ'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'おめでとう!\n${widget.num}個成功したよ',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (context) => QuestionPage()),
+                    (route) => false);
+              },
+              child: Text(
+                'もう一回プレイする',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
